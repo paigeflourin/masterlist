8106f57f5ca7c232ca5b5f9178cc40e3b0c34212
pageflourin <pageflourin@gmail.com>
2018-10-11 13:13:21 +0800

    chenged from email to login nme

--- src/webparts/qnAMasterList/services/QnAMasterListService.tsx
+++ src/webparts/qnAMasterList/services/QnAMasterListService.tsx
@@ -80,8 +80,8 @@ export class QnAMasterListService  implements IQnAMasterListService {
         //let promises = [];
         let resp;
         let promises = userInfo.map( u => {
-            console.log(u.user.Description);
-            return sp.web.siteUsers.getByEmail(u.user.Description).get().then(); //res => {
+            console.log(u.user);
+            return sp.web.siteUsers.getByLoginName(u.user.Key).get().then(); //res => {
               //  return res.Id;   
             //});
         });
